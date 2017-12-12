426a6f13aeeab938a5baef17bbc98df2e5e26656
rahul <agraharisrm@gmail.com>
2017-12-12 14:30:51 +0000

    new updates

---
+++ performanceEval.py
@@ -0,0 +1,32 @@
+class evalMetric:
+
+    def __init__(self, n_split, dataset, classifier, X, y):
+        self.n_split = n_split
+        self.dataset = dataset
+        self.classifier = classifier
+        self.X = X
+        self.y = y
+
+    def k_fold_cross_validation(self):
+
+        from sklearn.model_selection import KFold
+        import numpy as np
+        from classifiers import classifier
+        from sklearn.metrics import accuracy_score
+        kf = KFold(n_splits=self.n_split)
+        X = np.array(self.X)
+        y = np.array(self.y)
+        dataset = np.array(self.dataset)
+        score = 0
+        for train_indices, test_indices in kf.split(dataset):
+            x_train = [X[i] for i in train_indices]
+            x_test = [X[i] for i in test_indices]
+            y_train = [y[i] for i in train_indices]
+            y_test = [y[i] for i in test_indices]
+            clf = classifier(x_train, y_train)
+            dict = {"svm": clf.support_vector(), "randomforest": clf.random_forest(),
+                    "decisiontree": clf.decision_tree(),
+                    "gradientboosting": clf.gradient_boosting()}
+            score += accuracy_score(y_test, dict[self.classifier].predict(x_test))
+
+        return score / self.n_split
